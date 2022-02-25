1d45970e9e5472fbef77a12b9f83d5671e1eb872
Uladzislau Khamkou <kham@tuta.io>
2022-02-25 19:48:32 +0300

    implement check for vector linear independence

--- vectors.py
+++ vectors.py
@@ -1,4 +1,6 @@
 from numbersretthinked import Rational
+from human_api import Message, raise_messages
+import matrices
 
 
 class Vector:
@@ -19,3 +21,27 @@ class Vector:
 
     def __sub__(u, v):
         return Vector(*(u - v for u, v in zip(u, v)))
+
+
+def are_linearly_independent(vectors):
+    for i, v in enumerate(vectors):
+        if all(x == 0 for x in v):
+            yield "Vector {i + 1} is a zero vector. So the whole system is linearly dependent."
+            yield False
+    if len(vectors) < 2:
+        yield "There are not enough vectors to be linearly dependent."
+        yield True
+        return
+    yield "Let's build matrix out of these vectors."
+    m = matrices.Matrix(*vectors)
+    yield m
+    yield "Let's find determinant of the matrix."
+    determinant = [None]
+    yield from raise_messages(m.interactive_determinant(), determinant)
+    if determinant[0] == 0:
+        yield "The determinant is zero, so the vectors are linearly dependent."
+        yield False
+        return
+    yield "The determinant does not equals to zero, so the vectors are linearly independent."
+    yield True
+    return
