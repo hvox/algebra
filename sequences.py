2877964408d9dd3bd93cf43a4fa234f9643878bc
Uladzislau Khamkou <kham@tuta.io>
2022-05-19 18:57:29 +0300

    implement bychkovskiy's numbers

---
+++ sequences.py
@@ -0,0 +1,13 @@
+def bychkovskiy_number(x, k):
+    assert isinstance(k, int), "k must be integer!"
+    result = 1
+    for k in range(k, 0, 2 * (k < 0) - 1):
+        result = (x + k) ** result
+    return x**result
+
+
+BYCHKOVSKIY_NUMBER = 2**3**4
+if __name__ == "__main__":
+    x = 2
+    for n in range(-2, 10):
+        print(f"{x}_{n} ->", bychkovskiy_number(x, n))
