6a0667d7e97d7a030ccb181245b6c592510073cb
Uladzislau Khamkou <kham@tuta.io>
2022-09-30 19:06:38 +0300

    implement shrinked version of Shannon coding

--- compression_algorithms.py
+++ compression_algorithms.py
@@ -1,5 +1,6 @@
 from fractions import Fraction as Rat
 from math import log, ceil
+Encoding = list[tuple[int, ...]]
 
 
 def fraction_to_bin(number, digits: int) -> str:
@@ -11,7 +12,7 @@ def fraction_to_bin(number, digits: int) -> str:
     return "".join(representation)
 
 
-def shannon(frequencies: list[Rat], n: int = 2) -> list[tuple[int, ...]]:
+def shannon(frequencies: list[Rat], n: int = 2) -> Encoding:
     assert n == 2
     frequencies = [Rat(x) for x in reversed(sorted(frequencies))]
     whole, encoding, left = sum(frequencies), [], Rat(0)
@@ -20,6 +21,27 @@ def shannon(frequencies: list[Rat], n: int = 2) -> list[tuple[int, ...]]:
         encoding.append(tuple(map(int, fraction_to_bin(left / whole, digits))))
         left += freq
     return encoding
+
+
+def shrink_encoding(encoding: Encoding) -> Encoding:
+    trie, paths = {}, []
+    for code in encoding:
+        node, path = trie, []
+        for char in code:
+            node.setdefault(char, [0, {}])[0] += 1
+            path.append(node[char])
+            node = node[char][1]
+        paths.append(path)
+    for i, code in enumerate(encoding):
+        path = paths[i]
+        while len(path) > 2 and path[~1][0] == 1:
+            path.pop()
+        encoding[i] = code[:len(path)]
+    return encoding
+
+
+def shannon_shrinked(frequencies: list[Rat], n: int = 2) -> Encoding:
+    return shrink_encoding(shannon(frequencies, n))
 
 
 def _shannon_fano(frequencies: list[Rat], n: int = 2):
@@ -44,15 +66,20 @@ def _shannon_fano(frequencies: list[Rat], n: int = 2):
             yield (part,) + tail
 
 
-def shannon_fano(frequencies: list[Rat], n: int = 2) -> list[tuple[int, ...]]:
+def shannon_fano(frequencies: list[Rat], n: int = 2) -> Encoding:
     sorted_freqs = list(sorted(enumerate(frequencies), key=lambda x: -x[1]))
-    encoding: list[tuple[int, ...]] = [()] * len(sorted_freqs)
+    encoding: Encoding = [()] * len(sorted_freqs)
     for i, code in enumerate(_shannon_fano([f for _, f in sorted_freqs], n)):
         encoding[sorted_freqs[i][0]] = code
     return encoding
 
 
-probs = [0.36, 0.18, 0.18, 0.12, 0.09, 0.07]
-print(probs)
-print("Shannon: ", ["".join(map(str, t)) for t in shannon(probs)])
-print("Shannon-Fano: ", ["".join(map(str, t)) for t in shannon_fano(probs)])
+probs = [Rat(x) for x in "0.36 0.18 0.18 0.12 0.09 0.07".split()]
+print(" ".join(map(str, map(float, probs))))
+for name, f in (
+        ("Shannon", shannon),
+        ("Shannon(shrinked)", shannon_shrinked),
+        ("Shannon-Fano", shannon_fano),
+        ):
+    encoding = " ".join("".join(map(str, code)) for code in f(probs))
+    print(f"{name}:\n  " + encoding)
