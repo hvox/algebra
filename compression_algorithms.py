3e22bd905923266a00c7ceec770e61e7c052f422
Uladzislau Khamkou <kham@tuta.io>
2022-09-30 15:25:11 +0300

    implement pure Shannon coding

--- compression_algorithms.py
+++ compression_algorithms.py
@@ -1,3 +1,27 @@
+from fractions import Fraction
+from math import log, ceil
+
+
+def fraction_to_bin(number, digits: int) -> str:
+    number, representation = Fraction(number), []
+    for _ in range(digits):
+        number *= 2
+        representation.append("01"[number >= 1])
+        number %= 1
+    return representation
+
+
+def shannon(frequencies: list[int], n: int = 2) -> list[tuple[int, ...]]:
+    assert n == 2
+    frequencies = list(map(Fraction, reversed(sorted(frequencies))))
+    whole, encoding, left = sum(frequencies), [], 0
+    for freq in frequencies:
+        digits = ceil(-log(freq / whole, 2))
+        encoding.append(fraction_to_bin(left / whole, digits))
+        left += freq
+    return encoding
+
+
 def _shannon_fano(frequencies: list[int], n: int = 2):
     if len(frequencies) <= n:
         if len(frequencies) < 2:
@@ -30,4 +54,5 @@ def shannon_fano(frequencies: list[int], n: int = 2) -> list[tuple[int, ...]]:
 
 probs = [0.36, 0.18, 0.18, 0.12, 0.09, 0.07]
 print(probs)
-print(["".join(map(str, t)) for t in shannon_fano(probs)])
+print("Shannon: ", ["".join(map(str, t)) for t in shannon(probs)])
+print("Shannon-Fano: ", ["".join(map(str, t)) for t in shannon_fano(probs)])
